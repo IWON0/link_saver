34b8c29d4ad6c6b2102dca339247c4275414c2cd
alexey.stash <107249310+IWON0@users.noreply.github.com>
2025-04-30 01:57:23 +0300

    Update serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -5,10 +5,11 @@ from django.contrib.auth.password_validation import validate_password
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
+    date_joined = serializers.DateTimeField(read_only=True)
 
     class Meta:
         model = User
-        fields = ('email', 'password', 'password2')
+        fields = ('email', 'password', 'password2', 'date_joined')
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
@@ -17,7 +18,8 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create(
-            email=validated_data['email']
+            email=validated_data['email'],
+            username=validated_data['email']
         )
         user.set_password(validated_data['password'])
         user.save()
