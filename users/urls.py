9fd7e87d7e90c4e447ffc0ebe25bdf57f0148218
alexey.stash <107249310+IWON0@users.noreply.github.com>
2025-04-30 01:56:41 +0300

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from users.views import RegisterView, ChangePasswordView, ResetPasswordView
+from users.views import RegisterView, ChangePasswordView, ResetPasswordView, LoginView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('change-password/', ChangePasswordView.as_view(), name='change-password'),
     path('reset-password/', ResetPasswordView.as_view(), name='reset-password'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
